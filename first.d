afdc32b93af184b538ec6461af897bcdaf48b292
flaming0 <epiforce57@gmail.com>
2012-09-26 19:53:57 +0400

    bubble sort

--- first.d
+++ first.d
@@ -1,53 +1,27 @@
-import std.algorithm, std.conv, std.ctype, std.regex,
-    std.range, std.stdio, std.string;
+import std.stdio;
     
-struct PersonaData
+void bubbleSort(T)(T[] array)
 {
-    uint totalWordsSpoken;
-    uint[string] wordCount;
-}
-
-void addParagraph(string line, ref PersonaData[string] info)
-{
-    line = strip(line);
-    
-    auto sentence = std.algorithm.find(line, ". ");
-    if (sentence.empty)
-        return;
-        
-    auto persona = line[0 .. $ - sentence.lenght];
-    sentence = tolower(strip(sentence[2 .. $]));
-    
-    auto words = split(sentence, regex("[ \t,.;:?]+"));
-    
-    if (!(persona in info))
-        info[persona] = PersonaData();
-        
-    info[persona].totalWordsSpoken += words.length;
-    foreach (word; words) ++info[persona].wordCount[word];
-}
-
-void main()
-{
-    PersonaData[string] info;
-    
-    string currentParagraph;
-    foreach (line; stdin.byLine())
+    foreach (i; 0 .. array.length)
     {
-        if (line.startWith(" ") 
-                && line.length > 4
-                && isalpha(line[4]))
+        foreach (j; 0 .. array.length - i - 1)
         {
-            currentParagraph ~= line[3 .. $];
-        }
-        else if (line.startWith(" ") 
-                    && line.length > 2
-                    && isalpha(line[2]))
-        {
-            addParagraph(currentParagraph, info);
-            currentParagraph = to!string(line[2 .. $]);
+            if (array[j] > array[j + 1])
+            {
+                T temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
         }
     }
+}
     
-    printResults(info);
+void main()
+{
+    int[] arr = [ 7, 6, 5, 4, 3, 2, 1 ];
+    
+    bubbleSort(arr);
+    
+    foreach(it; arr)
+        writeln(it);
 }
